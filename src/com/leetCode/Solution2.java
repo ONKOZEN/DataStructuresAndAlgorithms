da78431ff8a4881effd4e3529ca353b95ea8c72c
ONKOZEN <33984451+ONKOZEN@users.noreply.github.com>
2021-09-28 21:30:48 +0800

    solution2-1

--- src/com/leetCode/Solution2.java
+++ src/com/leetCode/Solution2.java
@@ -3,34 +3,56 @@ package com.leetCode;
 public class Solution2 {
     public static void main(String[] args) {
         Solution2 s = new Solution2();
-        ListNode l1 = s.numtoListNode(2345);
-        ListNode l2 = s.numtoListNode(1111);
+        ListNode l1 = s.numtoListNode(9999999);
+        ListNode l2 = s.numtoListNode(9999);
 
 
-        s.soutList(s.addTwoNumbers(l1, l2));
+        s.soutList(s.addTwoNumbers0(l1, l2));
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode pre = new ListNode(0);
         ListNode cur = pre;
         int carry = 0;
-        while (l1 != null && l2 != null) {
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum /10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
+    public ListNode addTwoNumbers0(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
             int sum = x + y + carry;
             carry = sum / 10;
             sum = sum % 10;
             cur.next = new ListNode(sum);
+
             cur = cur.next;
-            if(l1!=null){
+            if (l1 != null)
                 l1 = l1.next;
-            }
-            if(l2!=null){
+            if (l2 != null)
                 l2 = l2.next;
-            }
-            if(carry==1){
-                cur.next = new ListNode(carry);
-            }
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
         }
         return pre.next;
     }
